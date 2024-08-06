c7200bead26a3dc55754278454963424c57cd69a
nagamadhun <nadakudurunagamadhu@gmail.com>
2024-08-06 14:24:01 -0400

    Added title in the header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './header.css'
 const Header = () => {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+        <div className="headerTitles">
+            <span className='headerTitleSm'>React & Node</span>
+            <span className='headerTitleLg'>Blog</span>
+        </div>
+        <img className='headerImage' src="https://images.unsplash.com/photo-1706264337390-7d7a116f17bc?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    </div>
   )
 }
 
