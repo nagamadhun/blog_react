caca19af42f9c7359fb10e552c12d68a200bc58e
nagamadhun <nadakudurunagamadhu@gmail.com>
2024-08-06 14:24:31 -0400

    Added posts

---
+++ src/post/Post.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './post.css'
+const Post = () => {
+  return (
+    <div className='post'>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Post
---
+++ src/posts/Posts.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './posts.css'
+
+const Posts = () => {
+  return (
+    <div className='posts'>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+export default Posts
